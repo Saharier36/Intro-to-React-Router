fa59caef2c909031022c08d04ec92ba9db85f515
Saharier Omi <gsaharier761@gmail.com>
2026-04-03 23:35:13 +0600

    load data using loader and useLoaderData

---
+++ src/components/Users2/Users2.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { use } from 'react';
+
+const Users2 = ({ userPromise }) => {
+    const users = use(userPromise)
+    console.log(users);
+  return <div></div>;
+};
+
+export default Users2;
