7e935ba2f4c9fd04a91bc333dee29157921b2f6d
Saharier Omi <gsaharier761@gmail.com>
2026-04-03 22:33:33 +0600

    explore outlet, link and navlink

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-
+Live Demo - [intro-to-react-router.surge.sh](https://intro-to-react-router.surge.sh)
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css'
+const Footer = () => {
+    return (
+      <div>
+        <small>thank you for visiting our website -</small>
+        <small>
+          <a href="/">terms</a>
+          <a href="/">conditions</a>
+          <a href="/">privacy</a>
+        </small>
+      </div>
+    );
+};
+
+export default Footer;
