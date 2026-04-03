df00fa3e00dc12a3abc472b6827b87cefe1d3056
Saharier Omi <gsaharier761@gmail.com>
2026-04-03 19:09:48 +0600

    explore nasted routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,30 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Root from './components/Root/Root.jsx';
+import Home from './components/Home/Home.jsx';
+import Mobiles from './components/Mobiles/Mobiles.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: Root,
+    children: [
+      {index: true, Component: Home},
+      {path: 'mobiles', Component: Mobiles}
+    ]
+  },
+  {
+    path: "about",
+    element: <div>About me here</div>,
+  },
+  {
+    path: "blogs",
+    element: <div>All my blogs</div>,
+  },
+  {
+    path: "app",
+    Component: App,
   },
 ]);
 
